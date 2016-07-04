32047dd09a853f25627be7132a59f7502432ba7a
John Morrice <john@functorama.com>
2016-07-04 10:18:10 +0100

    Done euler 5

--- five/five.go
+++ five/five.go
@@ -1,9 +1,80 @@
-package five
+package main
 
-func Euler5A(top int64) int64 {
-	return 0
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	n := flag.Int("n", 10, "The maximum factor")
+	flag.Parse()
+
+	fmt.Println(Euler5(*n))
+} 
+
+func Euler5(top int) int {
+	primes := primesunder(top)
+
+	maxpows := make([]int, len(primes))
+
+	for i := 2; i <= top; i++ {
+		for j, p := range primes {
+			rem := 0
+			div := i
+			pow := -1
+
+			for ; rem == 0 ; pow++ {
+				rem = div % p
+				div = div / p
+			}
+
+			prev := &maxpows[j]
+			
+			if *prev < pow {
+				*prev = pow
+			}
+		}
+	}
+
+	product := 1
+	for i, p := range primes {
+		pow := maxpows[i]
+
+		product *= intpow(p, pow)
+	}
+
+	return product
 }
 
-func Euler5B(top int64) int64 {
-	return 0
+func intpow(n int, m int) int {
+	if m == 0 {
+		return 1
+	}
+
+	pow := n
+	for i := 0; i < m - 1; i++ {
+		pow *= n
+	}
+
+	return pow
+}
+
+func primesunder(max int) []int {
+	primes := []int{2, 3}
+
+	for i := 4; i < max; i++ {
+		isprime := true
+		for _, p := range primes {
+			if i % p == 0 {
+				isprime = false
+				break
+			}
+		}
+
+		if isprime {
+			primes = append(primes, i)
+		}
+	}
+
+	return primes
 }
--- five/five_test.go
+++ five/five_test.go
@@ -1,15 +1,12 @@
-package five
+package main
 
 import (
 	"testing"
 )
 
 func TestEuler5A(t *testing.T) {
-	if Euler5A(10) != 2520 {
-		t.Error("Euler5(10) != 2520")
-	}
-
-	if Euler5B(10) != 2520 {
-		t.Error("Euler5B(10) != 2520")
+	actual := Euler5(10)
+	if actual != 2520 {
+		t.Error("Euler5(10) != 2520 was", actual)
 	}
 }
